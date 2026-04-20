b64c544306219ea1259c41af0b21a740b9c69845
AidenJeong8 <111260867+AidenJeong8@users.noreply.github.com>
2026-04-19 23:41:08 -0700

    Update convertImage.py

--- convertImage.py
+++ convertImage.py
@@ -1,6 +1,9 @@
 from PIL import Image
 import numpy as np
+from pathlib import Path
  
-path = "asl_alphabet_test/A_test.jpg"
+path = Path(__file__).parent / "asl_alphabet_test/A_test.jpg"
 img = Image.open(path).convert("RGB")
 matrix = np.array(img)
+ 
+print(matrix[0][0])
